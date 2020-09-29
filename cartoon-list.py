80d874a20953d02cad32758e9e52f8e2dec3d550
xuym <654122062@qq.com>
2020-09-29 21:14:04 +0800

    1

---
+++ cartoon-list.py
@@ -0,0 +1,43 @@
+import requests
+from bs4 import BeautifulSoup
+from PIL import Image
+import os
+from io import BytesIO
+import time
+import html_outputer
+
+class SpiderMain(object):
+    def __init__(self):
+        self.outputer = html_outputer.HtmlOutputer()
+
+    def craw(self, root_url):
+        headers = {
+            "User-Agent":
+            "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36"
+        }
+        r = requests.get(root_url, headers=headers)
+        soup = BeautifulSoup(r.content, 'html.parser')
+        items = soup.select("#chapter-list-1 li a")
+        folder_path = './photo'
+        if os.path.exists(folder_path) is False:
+            os.makedirs(folder_path)
+        for index, item in enumerate(items):
+            if item:
+              obj = {}
+              obj['url'] = item.get('href')
+              obj['title'] = item.get_text()
+              self.outputer.collect_data(obj)
+        self.outputer.output_html()
+
+
+if __name__ == "__main__":
+    root_url = "https://m.manhuafen.com/comic/39/"
+    obj_spider = SpiderMain()
+    obj_spider.craw(root_url)
+
+
+
+
+
+
+
